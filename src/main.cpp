febae2a582f9a2c32360e244fe7da33460471d86
Stefania <surregod@gmail.com>
2021-02-25 06:19:18 -0500

    Se agregaron los settings
    Se modificaron los settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
+#include <settings.h>
 
 #define VERSION "0.0.0"
 
 void setup() {
   // put your setup code here, to run once:
+  Settings::initSettings();
 }
 
 void loop() {
